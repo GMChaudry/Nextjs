799a5e8e25fd454653b7ccf22bd876293a976412
ghulam murtaza <g.m.chaudry@gmail.com>
2024-10-09 20:59:21 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+/*import { Fragment } from "react";
+
+const AboutPage =()=>{
+return <h1>about page</h1>
+}
+export default AboutPage;*/
+// pages/about.js
+export default function About() {
+    return <h1 style={{backgroundColor:"red"}}>About Page</h1>;
+}
---
+++ app/services/page.tsx
@@ -0,0 +1,29 @@
+// const ServicesPage =()=>{
+    //     return (
+    //             <div>This is Services page</div>
+    //     )
+    // }
+    // export default ServicesPage
+    
+   /* import React from 'react'
+    
+    function page() {
+      return (
+        <div>
+          
+        </div>
+      )
+    }
+    
+    export default page*/
+    // pages/services/index.js
+import Link from 'next/link';
+
+export default function Services() {
+    return (
+        <h1 style={{backgroundColor:"yellow"}}>
+            SERVICE PAGE
+        </h1>
+    );
+}
+    
